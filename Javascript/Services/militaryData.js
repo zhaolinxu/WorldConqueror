165f94830744282c8b90a546cfd1af3553bae12b
ArjaaAine <arjaaaine@gmail.com>
2014-10-26 01:47:11 -0700

    Lots of numbers
    Military numbers inputted.

--- Javascript/Services/militaryData.js
+++ Javascript/Services/militaryData.js
@@ -43,13 +43,16 @@ wciApp.factory('militaryData', function (myCountryData) {
 
 
 var setInitialUnitsData = function (units) {
+    //TODO: Later on add specific types of attacks like Air attack, land attack, piercing.. etc etc.
+
     units.baseStats = {
         army: [
             {
                 name: 'Militia',
                 description: 'desc',
                 cost: 1,
-                count: 1,
+                count: 0,
+                popCost: 1,
                 upkeep: 1,
 
                 attack: 1,
@@ -62,9 +65,10 @@ var setInitialUnitsData = function (units) {
                 description: 'desc',
                 cost: 10,
                 count: 0,
-                upkeep: 10,
+                popCost: 1,
+                upkeep: 5,
 
-                attack: 10,
+                attack: 5,
                 defense: 5,
                 siege: 0.1,
                 isUnlocked: false
@@ -72,49 +76,53 @@ var setInitialUnitsData = function (units) {
             {
                 name: 'Artillery',
                 description: 'desc',
-                cost: 1,
-                count: 1,
-                upkeep: 1,
+                cost: 10000,
+                count: 0,
+                popCost: 5,
+                upkeep: 2000,
 
-                attack: 1,
-                defense: 1,
-                siege: 0,
+                attack: 10,
+                defense: 1000,
+                siege: 5000,
                 isUnlocked: false
             },
             {
                 name: 'Anti-Air gun',
                 description: 'desc',
-                cost: 1,
-                count: 1,
-                upkeep: 1,
+                cost: 100000, //100k
+                count: 0,
+                popCost: 20,
+                upkeep: 10000,
 
-                attack: 1,
-                defense: 1,
-                siege: 0,
+                attack: 30000, //30k
+                defense: 1000,
+                siege: 1000,
                 isUnlocked: false
             },
             {
                 name: 'Tank',
                 description: 'desc',
-                cost: 1,
-                count: 1,
-                upkeep: 1,
+                cost: 10000000, //10m
+                count: 0,
+                popCost: 50,
+                upkeep: 750000, //750k
 
-                attack: 1,
-                defense: 1,
-                siege: 0,
+                attack: 1000000, //1m
+                defense: 1000000, //1m
+                siege: 1000000, //1m
                 isUnlocked: false
             },
             {
                 name: 'Landship',
                 description: 'desc',
-                cost: 1,
-                count: 1,
-                upkeep: 1,
+                cost: 100000000, //100m
+                count: 0,
+                popCost: 100,
+                upkeep: 1000000, //1m
 
-                attack: 1,
-                defense: 1,
-                siege: 0,
+                attack: 10000000, //10m
+                defense: 1000000, //1m
+                siege: 100000, //100k
                 isUnlocked: false
             },
         ],
@@ -122,73 +130,79 @@ var setInitialUnitsData = function (units) {
             {
                 name: 'Attack ship',
                 description: 'desc',
-                cost: 1,
-                count: 1,
-                upkeep: 1,
+                cost: 10,
+                count: 0,
+                popCost: 3,
+                upkeep: 10,
 
-                attack: 1,
-                defense: 1,
+                attack: 10,
+                defense: 10,
                 siege: 0,
                 isUnlocked: true
             },
             {
                 name: 'Submarine',
                 description: 'desc',
-                cost: 1,
-                count: 1,
-                upkeep: 1,
+                cost: 10000,
+                count: 0,
+                popCost: 20,
+                upkeep: 5000,
 
-                attack: 1,
-                defense: 1,
-                siege: 0,
+                attack: 8000,
+                defense: 1000,
+                siege: 2000,
                 isUnlocked: false
             },
             {
                 name: 'Destroyer',
                 description: 'desc',
-                cost: 1,
-                count: 1,
-                upkeep: 1,
+                cost: 100000, //100k
+                count: 0,
+                popCost: 100,
+                upkeep: 40000,
 
-                attack: 1,
-                defense: 1,
-                siege: 0,
+                attack: 10000,
+                defense: 100000, //100k
+                siege: 1000,
                 isUnlocked: false
             },
             {
-                name: 'Battlership',
+                name: 'Battleship',
                 description: 'desc',
-                cost: 1,
-                count: 1,
-                upkeep: 1,
+                cost: 10000000, //10m
+                count: 0,
+                popCost: 1000,
+                upkeep: 1000000, //1m
 
-                attack: 1,
-                defense: 1,
-                siege: 0,
+                attack: 1000000, //1m
+                defense: 1000000, //1m
+                siege: 3000000, //3m
                 isUnlocked: false
             },
             {
                 name: 'Cruiser',
                 description: 'desc',
-                cost: 1,
-                count: 1,
-                upkeep: 1,
+                cost: 100000000, //100m
+                count: 0,
+                popCost: 2000,
+                upkeep: 5000000, //5m
 
-                attack: 1,
-                defense: 1,
-                siege: 0,
+                attack: 10000000, //10m
+                defense: 20000000, //20m
+                siege: 5000000, //5m
                 isUnlocked: false
             },
             {
                 name: 'Aircraft Carrier',
                 description: 'desc',
-                cost: 1,
-                count: 1,
-                upkeep: 1,
+                cost: 1000000000, //1b
+                count: 0,
+                popCost: 10000,
+                upkeep: 25000000, //25m
 
-                attack: 1,
-                defense: 1,
-                siege: 0,
+                attack: 100000000, //100m
+                defense: 80000000, //80m
+                siege: 50000000, //50m
                 isUnlocked: false
             },
         ],
@@ -196,73 +210,79 @@ var setInitialUnitsData = function (units) {
             {
                 name: 'Fighter',
                 description: 'desc',
-                cost: 1,
-                count: 1,
-                upkeep: 1,
+                cost: 10000,
+                count: 0,
+                popCost: 5,
+                upkeep: 10000,
 
-                attack: 1,
-                defense: 1,
-                siege: 0,
+                attack: 10000,
+                defense: 10000,
+                siege: 1,
                 isUnlocked: true
             },
             {
                 name: 'Drone',
                 description: 'desc',
-                cost: 1,
-                count: 1,
-                upkeep: 1,
+                cost: 100000, //100k
+                count: 0,
+                popCost: 2,
+                upkeep: 50000,
 
-                attack: 1,
-                defense: 1,
-                siege: 0,
+                attack: 100000, //100k
+                defense: 10000,
+                siege: 1000,
                 isUnlocked: false
             },
             {
                 name: 'Helicopter',
                 description: 'desc',
-                cost: 1,
-                count: 1,
-                upkeep: 1,
+                cost: 1000000, //1m
+                count: 0,
+                popCost: 20,
+                upkeep: 200000, //200k
 
-                attack: 1,
-                defense: 1,
-                siege: 0,
+                attack: 100000, //100k
+                defense: 300000, //300k
+                siege: 300000, //300k
                 isUnlocked: false
             },
             {
                 name: 'Gunship',
                 description: 'desc',
-                cost: 1,
-                count: 1,
-                upkeep: 1,
+                cost: 10000000, //10m
+                count: 0,
+                popCost: 50,
+                upkeep: 1000000, //1m
 
-                attack: 1,
-                defense: 1,
-                siege: 0,
+                attack: 500000, //500k
+                defense: 2000000, //2m
+                siege: 2000000, //2m
                 isUnlocked: false
             },
             {
                 name: 'Bomber',
                 description: 'desc',
-                cost: 1,
-                count: 1,
-                upkeep: 1,
+                cost: 100000000, //100m
+                count: 0,
+                popCost: 5,
+                upkeep: 5000000, //5m
 
-                attack: 1,
-                defense: 1,
-                siege: 0,
+                attack: 10000000, //10m
+                defense: 100000, //100k
+                siege: 20000000, //20m
                 isUnlocked: false
             },
             {
                 name: 'Air force One.',
                 description: 'desc',
-                cost: 1,
-                count: 1,
-                upkeep: 1,
+                cost: 1000000000, //1b
+                count: 0,
+                popCost: 10000,
+                upkeep: 10000000, //10m
 
-                attack: 1,
-                defense: 1,
-                siege: 0,
+                attack: 10000000, //10m
+                defense: 100000000, //100m
+                siege: 10000000, //10m
                 isUnlocked: false
             },
         ]

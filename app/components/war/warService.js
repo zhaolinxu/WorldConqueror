e7be99f921e83c08a7c8dee7aa8ead51fed9ad0d
Tarnos <tarnos7@gmail.com>
2018-05-24 12:14:11 +0200

    Small changes

--- app/components/war/warService.js
+++ app/components/war/warService.js
@@ -11,13 +11,8 @@ wciApp.service('warService', function() {
 
     //Create objects for 2 sides, so it can be used with update method for actual fight
     War.prototype.initBattle = function (attacker, defender) {
-        //attacker and defender should be a simple object containing the country that attacked and unit list, so it can be saved easily.
-        //If attacker/defender already exists, we just combine them, unless we implement delay where attacking a target takes few turns to begin.
-        this.currentBattles.map(function (val) {
-            return {attacker: val.attacker, defender: val.defender}
-        }).indexOf({attacker: { code: 1}})
-        let warObject = {attacker:attacker, defender: defender};
-        this.currentBattles.unshift(warObject);
+        console.log(attacker);
+        console.log(defender);
     };
 
     War.prototype.doBattle = function () {
